ee30e4e43251cca161f8d8effa12264702d99bc2
Igor <izelenov7@mail.ru>
2025-05-06 12:49:48 +0300

    add show / close function

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.close-modal-window');
 const showModalWindows = document.querySelectorAll('.show-modal-window');
 
-const addHiddenClass = () => {
+const closeModalWindow = () => {
   modalWindow.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-const removeHiddenClass = () => {
+const showModalWindow = () => {
   modalWindow.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
 for (const item of showModalWindows) {
-  item.addEventListener('click', removeHiddenClass);
-  btnCloseModalWindow.addEventListener('click', addHiddenClass);
-  overlay.addEventListener('click', addHiddenClass);
+  item.addEventListener('click', showModalWindow);
+  btnCloseModalWindow.addEventListener('click', closeModalWindow);
+  overlay.addEventListener('click', closeModalWindow);
 };
