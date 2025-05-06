b6b4f05f3e5ba533e64697c205f14e15df64ce70
Igor <izelenov7@mail.ru>
2025-05-06 12:38:45 +0300

    add hidden and remove class function

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 'use strict';
 
-const modalWindow = document.querySelector('.show-modal-window');
-const closeWindow = document.querySelector('.close-modal-window');
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const showModalWindows = document.querySelectorAll('.show-modal-window');
+
+const addHiddenClass = () => {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const removeHiddenClass = () => {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (const item of showModalWindows) {
+  item.addEventListener('click', removeHiddenClass);
+  btnCloseModalWindow.addEventListener('click', addHiddenClass);
+  overlay.addEventListener('click', addHiddenClass);
+};
